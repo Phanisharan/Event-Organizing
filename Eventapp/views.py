d2f4052d14b052096df900157d036f5b8d70bcbd
PhaniSharan Boini <143081814+Phanisharan@users.noreply.github.com>
2025-02-12 23:26:39 +0530

    new

--- Eventapp/views.py
+++ Eventapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import *
 from .forms import ContactForm
 
